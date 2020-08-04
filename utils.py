faafb60757d0a18750c70a99993baaced47b0831
mr-morj <62052848+mr-morj@users.noreply.github.com>
2020-08-04 17:53:36 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -37,7 +37,7 @@ def select_predict():
     select, sеlect_step = select_type_learning(predict_size)
     value = st.button('Отримати прогноз')
     if value:
-            y_test, y_train, preds, mae, X_test, X_train, model = work_model(predict_size, select, sеlect_step)
+            y_test, y_train, preds, mae, X_test, X_train, model = work_model(predict_size, select, sеlect_step, 1)
             print_res(y_test, preds, mae, X_test, predict_size)
             predict_plot(X_train[-4*predict_size:], X_test, preds, y_test)
             
@@ -46,5 +46,5 @@ def select_interp():
     predict_size = select_predict_size()
     if predict_size>=3:
         with st.spinner('Навчаємо та тренуємо модель...'):
-            y_test, y_train, preds, mae, X_test, X_train, model = work_model(predict_size, "Звичайне навчання", 0)
+            y_test, y_train, preds, mae, X_test, X_train, model = work_model(predict_size, "Звичайне навчання", 0, 0)
         shap_plots(model, X_train, y_train)    
