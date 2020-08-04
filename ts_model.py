1bc2ea279d1451fe4a0b018c22ecbfc07e2df19a
mr-morj <62052848+mr-morj@users.noreply.github.com>
2020-08-04 17:14:22 +0300

    Update ts_model.py

--- ts_model.py
+++ ts_model.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from xgboost import XGBRegressor
 import warnings
 import shap
 from sklearn.metrics import mean_absolute_error
 from statsmodels.tsa.stattools import adfuller
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, TimeSeriesSplit
+import time
+import random
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.filterwarnings("ignore", category=RuntimeWarning) 
 pd.set_option('mode.chained_assignment', None)
@@ -41,14 +42,9 @@ def download_data_economic():
 def predict_plot(X_train, X_valid, preds, y_true):
     
     st.markdown("Проілюструємо отримані значення на **графіку**.")
-    #comparison of predictions and real values
     fig, axs = plt.subplots(1, figsize=(6,4))
     fig.suptitle(f'Прогноз/реальні значення: {len(X_valid)} днів')
 
-    #axs.plot(data_usd[X_train.index[0]:X_valid.index[-1]].index, 
-    #         data_usd[X_train.index[0]:X_valid.index[-1]].values, 
-    #         'g-', label='real')
-    
     axs.plot(data_usd_curr[X_valid.index[0]:X_valid.index[-1]].index, 
              data_usd_curr[X_valid.index[0]:X_valid.index[-1]].values, 
              'g-', label='Реальний курс')
@@ -63,44 +59,46 @@ def predict_plot(X_train, X_valid, preds, y_true):
     axs.axhline(y=y_true.mean(), linestyle='--', color='g', label='Реальне середнє')
     fig.autofmt_xdate()
     axs.legend(loc=2)
-    #plt.show()
     st.pyplot()
-    
-def create_shift(s, windows=[7,8,9,10]):#,,14,20,30,50
+
+def create_shift(s, windows=[7,8,9,10]):
     
     cf = pd.DataFrame()
 
     for w in windows:    
         cf['shift_' + str(w)] = s.shift(w)
-        #cf['signal_shift_-' + str(w)] = s.shift(-1*w)
     
     for w in windows[1:]:
         cf['diff_shift_'+ str(w)] = -cf[f'shift_{windows[0]}'] + cf[f'shift_{w}']
+    
     len_rost = 0
     len_spad = 0
     rost_spad = [0] * (windows[0]+1)
+    
+    check = cf[f'shift_{windows[0]}']
     for i in range(windows[0]+1, len(s)):
-        if (s[i] > s[i-1]):
+        if (check[i] > check[i-1]):
+            
             len_rost = len_rost + 1
             len_spad = 0
             rost_spad.append(len_rost)
-        if (s[i] <= s[i-1]):
+        elif (check[i] <= check[i-1]):
+            
             len_spad = len_spad + 1
             len_rost = 0
             rost_spad.append(len_spad)
+
     cf['period_trend'] = rost_spad
     
     value = [0] * (windows[0]+1)
     for i in range(windows[0]+1, len(s)):
-        value.append(s[i] - s[i-1])
+        value.append(check[i] - check[i-1])
     cf['day_increase'] = value
-     
-    #cf['signal_power'] = s ** 2
-    #cf = cf.fillna(value=0) 
     
     return cf
 
-def calc_roll_stats(s, windows=[3, 5, 7, 10]):#, 20,30,50
+
+def calc_roll_stats(s, windows=[3, 5, 7, 10]):
 
     roll_stats = pd.DataFrame()
     
@@ -111,18 +109,6 @@ def calc_roll_stats(s, windows=[3, 5, 7, 10]):#, 20,30,50
         roll_stats['roll_max_' + str(w)] = s.rolling(window=w, min_periods=1).max()
         roll_stats['roll_q25_' + str(w)] = s.rolling(window=w, min_periods=1).quantile(0.25)
         roll_stats['roll_q75_' + str(w)] = s.rolling(window=w, min_periods=1).quantile(0.75)
-        #roll_stats['roll_range_' + str(w)] = roll_stats['roll_max_' + str(w)] - roll_stats['roll_min_' + str(w)]
-        '''
-        
-        roll_stats['roll_mean_s_' + str(w)] = s.rolling(window=w, min_periods=1).mean().shift(-w)
-        roll_stats['roll_std_s_' + str(w)] = s.rolling(window=w, min_periods=1).std().shift(-w)
-        roll_stats['roll_min_s_' + str(w)] = s.rolling(window=w, min_periods=1).min().shift(-w)
-        roll_stats['roll_max_s_' + str(w)] = s.rolling(window=w, min_periods=1).max().shift(-w)
-        roll_stats['roll_range_s_' + str(w)] = roll_stats['roll_max_s_' + str(w)] - roll_stats['roll_min_s_' + str(w)]
-
-        #roll_stats['kurtosis_' + str(w)] = roll_stats.apply(lambda x: pd.Series(x).kurtosis())
-        '''
-    #roll_stats = roll_stats.fillna(value=0)
              
     return roll_stats
 
@@ -130,13 +116,10 @@ def statictic_info(df, pr, windows=[30,50,60]):
 
     for w in windows:
         df['window_stationarity_' + str(w)] = [0]*len(df)
-        #df['window_stationarity_critical_value_' + str(w) + str(pr)] = [0]*len(df)
         n = int(len(df)/w) + 1
         for ci in range(n-1):
             P_result = adfuller(df[f'shift_{pr}'][ci*w:(ci+1)*w])
-            #print(s[ci*w:(ci+1)*w])
             df['window_stationarity_' + str(w)][ci*w:(ci+1)*w] = P_result[1]
-            #df['window_stationarity_critical_value_' + str(w) + str(pr)][ci*w:(ci+1)*w] = P_result[0]
     return df
 
 
@@ -174,18 +157,94 @@ def shap_plots(model, X, y_train):
             plt.clf()
         
         st.markdown("На графіку вище показані значення функцій. Значення SHAP представлені довжиною конкретної смуги. Однак, не зовсім зрозуміло, яке саме значення кожного SHAP _(це можна побачити нижче, якщо потрібно)_:")
-        #agree = st.button('Переглянути всі значення SHAP')
         if st.checkbox('Переглянути всі значення SHAP'):
             shap_table=pd.DataFrame(shap_values,columns=X.columns)
             st.table(shap_table.iloc[individual])
-    
 
+def calc(X,y,model,cv, pred_size):
+    res=[]
+    for i, (tdx, vdx) in enumerate(cv.split(X, y)):
+        
+        X_train, X_valid, y_train, y_valid = X.iloc[tdx], X.iloc[vdx], y[tdx], y[vdx]
+        model.fit(X_train, y_train, verbose=False)   
+        preds = model.predict(X_valid)
+        mae = mean_absolute_error(y_valid, preds)
+        res.append(mae)
+    '''
+    X_train_valid, X_test_valid, y_train_valid, y_test_valid = train_test_split(X, y, test_size=5*pred_size/len(X), random_state=seed, shuffle=False)
+        
+    model.fit(X_train_valid, y_train_valid, verbose=False)   
+
+    preds = model.predict(X_test_valid)   
+    mae = mean_absolute_error(y_test_valid, preds)
+    res.append(mae)
+    '''
+    return np.mean(res) 
+
+def true_select(s):
+    '''
+    highlight the maximum in a Series yellow.
+    '''
+    color = 'limegreen' if s =='Відібрана' else 'tomato'
+    return 'background-color: %s' % color
+    #return ['background-color: green' if v else '' for v in is_max]
+    
+def feature_selection_forward(train_num, y, model, size):
+    
+    st.markdown("Всі результати автоматично занесуться до таблиці з відповідними значеннями.")
+    check_df = pd.DataFrame({}, columns=['Функція', 'Результат'])
+    my_table = st.table(check_df)
+    #time.sleep(10)
+    
+    train_num = train_num[-size*20:]
+    y = y[-size*20:]
+    #tscv = TimeSeriesSplit(n_splits=12)
+    tscv = TimeSeriesSplit(n_splits=3)
+    baseline=100000
+    train_clear = pd.DataFrame()
+    while True:
+        
+        cols = list(set(train_num.columns) - set(train_clear.columns))
+        add_rmse = []
+        
+        for add_col in cols:
+            train_add=train_clear.copy()
+            train_add[add_col] = train_num[add_col]
+            
+            add_rmse.append(calc(train_add,y,model,tscv, size))
+
+        add_df = pd.DataFrame(list(zip(add_rmse, cols)), columns =['MAE', 'Column'])
+        add_df=add_df.sort_values(by=['MAE'], ascending=True)
+        print('\n', add_df.head())
+        
+        if baseline > add_df.iloc[0, 0]:
+            baseline = add_df.iloc[0, 0]
+            print(add_df.iloc[0, 1])
+            train_clear[add_df.iloc[0, 1]] = train_num[add_df.iloc[0, 1]]
+            print(f"MAE: {round(baseline,8)}, num_cols:{len(train_clear.columns)}, cols:{list(train_clear.columns)} \n") 
+        else:
+            print('nothing better')
+            l = random.sample(list(train_num.columns), len(train_num.columns))
+            for i in l:
+                t = 'Не відібрана'
+                if i in list(train_clear.columns):
+                    t = 'Відібрана'
+                upd = pd.DataFrame({'Функція': [i], 'Результат': [t]})
+                upd = upd.style.applymap(true_select, subset=['Результат'])
+                my_table.add_rows(upd)
+                time.sleep(0.8)
+            break
+        
+        
 data_usd_curr = download_data_usd()
 
 
 def work_model(predict_size, select, sеlect_step):
     
-    model = XGBRegressor(objective ='reg:squarederror', random_state=seed)        
+    model = XGBRegressor(eval_metric='mae',
+                             learning_rate=0.05,n_estimators=200,
+                             max_depth=2,min_child_weight=3,
+                             subsample=0.7,colsample_bytree=0.5, random_state=seed)      
     if (select =="Звичайне навчання"):
         data_usd = data_usd_curr.copy()     
         shift = create_shift(data_usd, [predict_size, predict_size+2, predict_size+4])
@@ -198,43 +257,22 @@ def work_model(predict_size, select, sеlect_step):
         X = X.dropna()
         
         X = statictic_info(X, predict_size, [7, 14, 30])
-        #X["weekday"] = X.index.weekday
-        #X['is_weekend'] = X.weekday.isin([5,6])*1
         
         y = X.y
         X = X.drop(labels=['y'], axis=1)
-
         X_train = X[:-predict_size]
         X_test = X[-predict_size:]
         y_train = y[:-predict_size]
-        #y_train = np.log(y_train)
         y_test = y[-predict_size:]
+        with st.spinner('Проводимо відбір функцій...'):
+            feature_selection_forward(X_train, y_train, model, predict_size)
 
-        X_train_valid, X_test_valid, y_train_valid, y_test_valid = train_test_split(X_train, y_train, test_size=predict_size/len(X_train), random_state=seed, shuffle=False)
-        
-        model.fit(X_train_valid, y_train_valid,
-                        eval_set=[(X_train_valid, y_train_valid), (X_test_valid, y_test_valid)],
-                        early_stopping_rounds=50,
-                       verbose=False)   
-
-        preds = model.predict(X_test_valid)
-        #preds = np.exp(preds)
-        #y_test_valid = np.exp(y_test_valid)
-        mae = mean_absolute_error(y_test_valid, preds)
-        #print(f'MAE on valid set: {mae} | predict size: {predict_size}')
-        
-        model.fit(X_train, y_train, verbose=False)
-
-        preds = model.predict(X_test)
-        #preds = np.exp(preds)
-        mae = mean_absolute_error(y_test, preds)
-        #print(f'MAE on test set: {mae} | predict size: {predict_size}')
-        
-        #st.write('MAE on test set: ', mae, ' | predict size: ', predict_size)
-        
-        ####predict_plot(X_train[-4*predict_size:], X_test, preds)
-
-        #print('===========SV LR complete===============')
+        with st.spinner('Очікуйте результати...'):    
+            X_train_valid, X_test_valid, y_train_valid, y_test_valid = train_test_split(X_train, y_train, test_size=predict_size/len(X_train), random_state=seed, shuffle=False)
+            
+            model.fit(X_train, y_train, verbose=False)
+            preds = model.predict(X_test)
+            mae = mean_absolute_error(y_test, preds)
         
     else:
         predict_lin = predict_size
@@ -251,8 +289,6 @@ def work_model(predict_size, select, sеlect_step):
         X['y'] = y
         X = X.dropna()
         X = statictic_info(X, step, [7, 14, 30])
-        #X["weekday"] = X.index.weekday
-        #X['is_weekend'] = X.weekday.isin([5,6])*1
         
         y = X.y
         X = X.drop(labels=['y'], axis=1)
@@ -261,72 +297,71 @@ def work_model(predict_size, select, sеlect_step):
         X_test = X[-predict_size:]
         y_train = y[:-predict_size]
         y_test = y[-predict_size:]
+        with st.spinner('Проводимо відбір функцій...'):
+            feature_selection_forward(X_train, y_train, model, step)
         
         preds_full = []
         y_test_full = y_test.copy()
         y_train_full = y_train.copy()
         X_test_full = X_test.copy()
         X_train_full = X_train.copy()
-        numb_steps = 0
-        while (predict_size>=step):
-            predict_size = predict_size - step
-            numb_steps = numb_steps + 1
-        #print(numb_steps, 'full steps')
-            
-        last_step_size = predict_size
         
-        for stt in range(1, numb_steps+1): 
+        with st.spinner('Очікуйте результати...'):
+        
+            numb_steps = 0
+            while (predict_size>=step):
+                predict_size = predict_size - step
+                numb_steps = numb_steps + 1
+                
+            last_step_size = predict_size
             
-            model.fit(X_train, y_train, verbose=False)
-
-            preds = model.predict(X_test[:step])
-            mae = mean_absolute_error(y_test[:step], preds)
-            #print(f'MAE on test set: {mae} | days: {step*stt}')
-            #predict_plot(X_train_full[-4*predict_lin:], X_test_full[:step], preds)
-            
-            X_train = X_train.append(X_test[:step])
-            
-            pred_series = pd.Series(list(preds), index=[X_test[:step].index])
-            y_train = np.append(y_train, pred_series)
-            preds_full = preds_full+ preds.tolist()
-            
-            X_test = X_test.iloc[step:]#.reset_index(drop=True)
-            y_test = y_test[step:]
-            
-            X_train['y'] = y_train
-            X_test['y'] = y_test
-            X_reinf = pd.concat([X_train, X_test], axis=0)
-            
-            data_usd_reinf = X_reinf.y
-            X_reinf = X_reinf.drop(labels=['y'], axis=1)
-
-            shift = create_shift(data_usd_reinf, [step, step+2, step+4])
-            shift = shift.astype(float)
-            roll_stats = calc_roll_stats(shift[f'shift_{step}'], [step, step+2, step+4, step+6])
-            
-            X_lag_reinf = pd.concat([shift, roll_stats], axis=1)
-            y_lag_reinf = data_usd_reinf.values
-            
-            X_train = X_lag_reinf.iloc[:len(X_train)]#.reset_index(drop=True)
-            X_test = X_lag_reinf.iloc[len(X_train):]#.reset_index(drop=True)
-            y_train = y_lag_reinf[:len(y_train)]
-            y_test = y_lag_reinf[len(y_train):]
-            
-            
-        if last_step_size>0:
-            model.fit(X_train, y_train, verbose=False)
-            preds = model.predict(X_test)
-            pred_series = pd.Series(list(preds), index=[X_test.index])
-            y_train = np.append(y_train, pred_series)
-            preds_full = preds_full+ preds.tolist()
-            mae = mean_absolute_error(y_test_full, y_train[-len(y_test_full):])
-        #print(f'full MAE on test set: {mae} | predict size: {predict_lin}')
-        #st.write('full MAE on test set: ', mae, ' | predict size: ', predict_lin)
-        X_test = X_test_full
-        X_train_full = X_train
-        y_test = y_test_full
-        y_train = y_train_full
-        preds = np.array(preds_full)
-        ####predict_plot(X_train_full[-4*predict_lin:], X_test_full, preds_full)
+            for stt in range(1, numb_steps+1): 
+                
+                model.fit(X_train, y_train, verbose=False)
+    
+                preds = model.predict(X_test[:step])
+                mae = mean_absolute_error(y_test[:step], preds)
+                
+                X_train = X_train.append(X_test[:step])
+                
+                pred_series = pd.Series(list(preds), index=[X_test[:step].index])
+                y_train = np.append(y_train, pred_series)
+                preds_full = preds_full+ preds.tolist()
+                
+                X_test = X_test.iloc[step:]
+                y_test = y_test[step:]
+                
+                X_train['y'] = y_train
+                X_test['y'] = y_test
+                X_reinf = pd.concat([X_train, X_test], axis=0)
+                
+                data_usd_reinf = X_reinf.y
+                X_reinf = X_reinf.drop(labels=['y'], axis=1)
+    
+                shift = create_shift(data_usd_reinf, [step, step+2, step+4])
+                shift = shift.astype(float)
+                roll_stats = calc_roll_stats(shift[f'shift_{step}'], [step, step+2, step+4, step+6])
+                
+                X_lag_reinf = pd.concat([shift, roll_stats], axis=1)
+                y_lag_reinf = data_usd_reinf.values
+                
+                X_train = X_lag_reinf.iloc[:len(X_train)]
+                X_test = X_lag_reinf.iloc[len(X_train):]
+                y_train = y_lag_reinf[:len(y_train)]
+                y_test = y_lag_reinf[len(y_train):]
+                
+                
+            if last_step_size>0:
+                model.fit(X_train, y_train, verbose=False)
+                preds = model.predict(X_test)
+                pred_series = pd.Series(list(preds), index=[X_test.index])
+                y_train = np.append(y_train, pred_series)
+                preds_full = preds_full+ preds.tolist()
+                mae = mean_absolute_error(y_test_full, y_train[-len(y_test_full):])
+            X_test = X_test_full
+            X_train_full = X_train
+            y_test = y_test_full
+            y_train = y_train_full
+            preds = np.array(preds_full)
         
     return y_test, y_train, preds, mae, X_test, X_train, model
